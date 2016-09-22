2c00a6deaf6357469c2adcf0210e54edd8fcf35b
Sunpil Kim <ezweb28@gmail.com>
2016-09-22 00:41:40 -0400

    Continuing

--- M4/src/fxapp/MainFXApplication.java
+++ M4/src/fxapp/MainFXApplication.java
@@ -1,19 +1,79 @@
 package fxapp;
 
+import controller.MainScreenController;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MainFXApplication extends Application {
+    /** the java logger for this class */
+    private static final Logger LOGGER = Logger.getLogger("MainFXApplication");
+
+
+
+    /** the main container for the application window */
+    private Stage mainScreen;
+
+    /** the main layout for the main window */
+    private BorderPane rootLayout;
+
+    /**
+     * return a reference to the main window stage
+     * @return reference to main stage
+     * */
+    public Stage getMainScreen() {
+        return mainScreen;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        mainScreen = primaryStage;
+//        initRootLayout(mainScreen);
         Parent root = FXMLLoader.load(getClass().getResource("../view/WelcomeScreen.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+    }
+
+    /**
+     * Initialize the main screen for the application.  Most other views will be shown in this screen.
+     *
+     * @param mainScreen  the main Stage window of the application
+     */
+    private void initRootLayout(Stage mainScreen) {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainFXApplication.class.getResource("../view/WelcomeScreen.fxml"));
+            rootLayout = loader.load();
+
+            // Give the controller access to the main app.
+            MainScreenController controller = loader.getController();
+            controller.setMainApp(this);
+
+            // Set the Main App title
+            mainScreen.setTitle("Welcome!");
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            mainScreen.setScene(scene);
+            mainScreen.show();
+
+
+        } catch (IOException e) {
+            //error on load, so log it
+            LOGGER.log(Level.SEVERE, "Failed to find the fxml file for MainScreen!!");
+            e.printStackTrace();
+        }
     }
 
 
