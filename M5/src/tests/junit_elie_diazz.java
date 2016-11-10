aed7504cd405e5743d819dce8fd8b291bb7b0fa6
ElienDiaz <eliericardo@hotmail.com>
2016-11-09 21:15:17 -0500

    PLS

--- M5/src/tests/junit_elie_diazz.java
+++ M5/src/tests/junit_elie_diazz.java
@@ -1,7 +1,62 @@
 package tests;
 
+import model.Model;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Elie Diaz on 11/9/2016.
  */
 public class junit_elie_diazz {
+    /**
+     * The test things
+     */
+    Model current;
+
+    /**
+     * This method is run before each test
+     * We know in the report there exists a report where id: 962704 reportername: jiacino3
+     * We will test it with 4 ways
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        current = Model.getInstance();
+    }
+
+
+    /**
+     * test findreport for null id and user value
+     */
+    @Test
+    public void testNullReport() {
+        Assert.assertNull("Report should be null!",current.findReport(null, 0));
+    }
+
+    /**
+     * test findReport with id that doesn't exist
+     */
+    @Test
+    public void testWrongIdNumber() {
+        Assert.assertNull("Report should be null", current.findReport("jiacino3",1234));
+    }
+
+    /**
+     * test findAccount with id that doesn't exist
+     */
+    @Test
+    public void testWrongReportName() {
+        Assert.assertNull("Report should be null", current.findReport("user",962704));
+    }
+
+    /**
+     * test findAccount with id and user that do exist
+     */
+    @Test
+    public void testCorrectIdUserReport() {
+        Assert.assertEquals("Existing report not found!", "33.85, -88.39",
+                current.findReport("jiacino3", 962704).getLocation());
+    }
+
 }
