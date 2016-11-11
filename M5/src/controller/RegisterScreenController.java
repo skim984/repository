05764b643c4615dd4ded4733021b36bffbe9444d
Sunpil Kim <ezweb28@gmail.com>
2016-11-10 19:23:13 -0500

    Add ID validation in RegisterScreenController

--- M5/src/controller/RegisterScreenController.java
+++ M5/src/controller/RegisterScreenController.java
@@ -97,7 +97,8 @@ public class RegisterScreenController {
         char ch[] = str.toCharArray();
         for(int i = 0; i < ch.length; i++) {
             if (!((48 <= (int)ch[i] && 57 >= (int)ch[i]) ||
-                    (97 <= (int)ch[i] && 122 >= (int)ch[i]))) {
+                    (97 <= (int)ch[i] && 122 >= (int)ch[i]) ||
+                    (65 <= (int)ch[i] && 90 >= (int)ch[i]))) {
                 errorMessage += ch[i] + " is invalid account name";
                 i = ch.length;
             }
